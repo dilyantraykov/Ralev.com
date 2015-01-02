aafdd8049eb3f0a127a0a694eb9834ab2327b746
Dilyan Traykov <dtraykov94@gmail.com>
2015-01-03 01:54:11 +0200

    Using Git inside of Sublime with Emmet

---
+++ todo_app/test.php
@@ -0,0 +1,30 @@
+<!-- Code created only with Emmet  -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="navbar-header"><a href="" class="navbar-brand">Crazy Fast</a></div>
+  <ul class="nav navbar-nav">
+    <li class="menu-1"></li>
+    <li class="menu-2"></li>
+    <li class="menu-3"></li>
+  </ul>
+</nav>
+<div class="jumbotron text-center">
+  <h1></h1>
+  <p></p>
+  <a href="" id="go-button" class="btn btn-danger"></a>
+</div>
+<div class="jumbotron text-center">
+  <h1></h1>
+  <p></p>
+  <a href="" id="go-button" class="btn btn-danger"></a>
+</div>
+<div class="test"></div>
+  
+</body>
+</html>
